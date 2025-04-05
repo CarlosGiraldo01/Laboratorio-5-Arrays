262d881e63821e9fc723b3417eb0f24192046ab5
CarlosGiraldo01 <1109117326@u.icesi.edu.co>
2025-04-04 19:15:03 -0500

    avances

--- ejercicio2.js
+++ ejercicio2.js
@@ -1,18 +1,43 @@
-let frutas = ["manzana", "banano", "uva", "pera", "sandia", "kiwi", "naranja"]
-console.log(frutas);
+
+let frutas = ['manzana', 'banano', 'uva', 'pera', 'sandía', 'kiwi', 'naranja'];
+console.log('Arreglo original:', frutas);
 
 frutas.sort();
-console.log(frutas);
+console.log('Ordenado alfabéticamente:', frutas);
 
-function singularAPlural(palabra) {
-   palabra = palabra + "s"
-    return palabra
+function SingularAPlural(palabra) {
+    if (!palabra.endsWith('s')) {
+        return palabra + 's';
+    }
+    return palabra;
 }
 
-let FrutasMayus = []
-for (const fruta of frutas) {
-
-    FrutasMayus.push(singularAPlural(fruta))
-    Frutas.endsWith('s')
+let frutasPlural = [];
+for (let fruta of frutas) {
+    frutasPlural.push(SingularAPlural(fruta));
 }
-console.log(FrutasMayus);
+console.log('Frutas plural:', frutasPlural);
+
+function existeFruta(fruta) {
+    return frutas.includes(fruta);
+}
+console.log('¿Existe uva?', existeFruta('uva')); 
+console.log('¿Existe mango?', existeFruta('mango')); 
+
+let frutasCortas = frutas.filter(fruta => fruta.length <= 4);
+console.log('Frutas cortas (<= 4 letras):', frutasCortas);
+
+
+function eliminaFruta(fruta) {
+    let index = frutas.indexOf(fruta);
+    if (index !== -1) {
+        frutas.splice(index, 1); 
+        return frutas;
+    } else {
+        return false; 
+    }
+}
+console.log('Eliminar kiwi:', eliminaFruta('kiwi'));
+console.log('Eliminar mango:', eliminaFruta('mango'));
+
+console.log('Arreglo final:', frutas);
